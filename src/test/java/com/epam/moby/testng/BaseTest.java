4cbe2920800a93ef07ffa0f750f6047ca1857b85
Anastasia Murashko <anastasiya_murashka@epam.com>
2021-07-10 18:30:28 +0300

    Create BaseTest.java

---
+++ src/test/java/com/epam/moby/testng/BaseTest.java
@@ -0,0 +1,25 @@
+package com.epam.moby.testng;
+
+import com.epam.moby.testng.driver.DriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.testng.annotations.AfterMethod;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void createSession(){
+        DriverManager.getDriver();
+    }
+
+    @AfterMethod
+    public void resetApp(){
+        DriverManager.getDriver().resetApp();
+    }
+
+    @AfterClass
+    public void closeSession(){
+        DriverManager.closeDriver();
+        DriverManager.closeEmulator();
+    }
+}
